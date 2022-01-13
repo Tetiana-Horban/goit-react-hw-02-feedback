ce4d3545a40c335d7cf6ecd76984bd86862b0110
Tetiana-Horban <tanya.ryptyk@gmail.com>
2022-01-13 02:01:14 +0200

    add styled for notification message

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
-
-const NotificationMessage = styled.p`
-  font-size: 25px;
-  text-align: center;
-  color: #7e5a17;
-`;
+import NotificationMessage from './Notification.styled';
 
 const Notification = ({ message }) => {
   return <NotificationMessage>{message}</NotificationMessage>;
